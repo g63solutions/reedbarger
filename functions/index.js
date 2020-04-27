aad367e234d075135784323576598d6425de7482
g63solutions <g63solutions@gmail.com>
2020-04-27 00:37:44 -0400

    Committed

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,27 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+//This Is The Trigger
+exports.onCreateFollower = functions.firestore
+    .document('/followers/{userId}/userFollowers/{followerId}')
+    .onCreate((snapshot, context) => {
+//After onCreate you get the userId & followerId
+        const userId = context.params.userId;
+        const followerId = context.params.followerId;
+
+//admin.firestore() is the same as Firestore.instance
+// 1) Create Followed Users Posts
+        const followedUserPostsRef = admin
+            .firestore().collection('posts')
+            .collection('posts')
+            .doc(userId)
+            .collection('userPosts');
+
+// 2) Create  Following Users Timeline Ref
+        const timelinePostsRef = admin
+            .firestore()
+            .collection('timeline')
+            .doc(followerId)
+            .collection('timelinePosts');
+})
